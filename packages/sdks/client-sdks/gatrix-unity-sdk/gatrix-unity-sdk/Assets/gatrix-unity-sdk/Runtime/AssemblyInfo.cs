2c632984e0f9e723e8a8e694ef17e4f47c3df757
jhseo <maxidea1024@gmail.com>
2026-02-19 14:04:37 +0900

    chore(unity): add sample project files

--- packages/sdks/client-sdks/gatrix-unity-sdk/gatrix-unity-sdk/Assets/gatrix-unity-sdk/Runtime/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Assembly-level attributes for Gatrix.Unity.SDK
-// Grants Editor and Test assemblies access to internal types
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Gatrix.Unity.SDK.Editor")]
-[assembly: InternalsVisibleTo("Gatrix.Unity.SDK.Tests")]
